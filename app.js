c7e37c79a3391197ad94e369c12f3b52574c4384
richieyu <richieyu@tencent.com>
2022-09-04 09:30:22 +0800

    tail -f backend support finished; TODO frontend support

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var formidable = require('formidable'),
     fs = require('fs'),
     process = require('process'),
     path = require('path');
+const { stdout } = require('process');
 
 const BIND_IP = '0.0.0.0';
 const BIND_PORT = 8080;
@@ -48,19 +49,21 @@ function parseHttpReqRange(rangeLine, fileSize) {
 
 function mkStatPromise(dirPath, fileName) {
     return new Promise((resolve) => {
-        const fileRelePath = path.join(dirPath, fileName);
-        fs.stat(fileRelePath, (err, stat) => {
+        const fileRelPath = path.join(dirPath, fileName);
+        fs.stat(fileRelPath, (err, stat) => {
             if (err != null) {
-                console.warn(`directory: stat(${fileRelePath}) failed: ${err.message}`);
+                console.warn(`directory: stat(${fileRelPath}) failed: ${err.message}`);
                 resolve({ 'name': fileName });
             } else {
                 if (stat.isDirectory()) {
                     fileName += '/';
                 }
+                const href = encodeURI(path.join('/', dirPath, fileName));
                 resolve({
                     'name': fileName,
-                    'href': encodeURI(path.join('/', dirPath, fileName)),
-                    'mtime': stat.mtime.toLocaleString( 'sv', { timeZoneName: 'short' } ),
+                    'href': href,
+                    'hrefStringified': (stat.isFile() ? JSON.stringify(href) : null),
+                    'mtime': stat.mtime.toLocaleString('sv', { timeZoneName: 'short' }),
                     'size': stat.size,
                     'sizeHumanReadable': humanFileSize(stat.size),
                 });
@@ -69,9 +72,8 @@ function mkStatPromise(dirPath, fileName) {
     });
 }
 
-console.log(`starting http server at ${BIND_IP}:${BIND_PORT}...`);
-http.createServer(async function (req, rsp) {
-    let decodedUrlPath = decodeURI((new URL(req.url, 'scheme://host')).pathname);
+async function parseUriToFileStat(uri) {
+    let decodedUrlPath = decodeURI((new URL(uri, 'scheme://host')).pathname);
     // now: decodedUrlPath possible values(tailing slash is not always carried): '/', '/foo', '/foo/', '/bar.txt'
     if (decodedUrlPath.endsWith('/')) {
         // make sure there is no trailing slash, resulting like: '', '/foo', '/bar.txt'
@@ -81,16 +83,35 @@ http.createServer(async function (req, rsp) {
     // now: fileRelPath possible values: '.', 'foo', 'bar.txt'
     // path.resolve(fileRelPath) possible values(tailing slash is never carried): '/cwd', '/cwd/foo', '/cwd/bar.txt'
     if (path.resolve(fileRelPath) != path.join(path.resolve(process.cwd()), decodedUrlPath)) {
-        rsp.writeHead(403, { 'Content-Type': "text/plain; charset=UTF-8" });
-        rsp.end("Not allowed to break the jail");
-        return;
+        return [null, null, null, {
+            code: 403,
+            header: { 'Content-Type': "text/plain; charset=UTF-8" },
+            message: "Not allowed to break the jail",
+        }];
     }
-    const [err, fileStat] = await new Promise((resolve) => {
+    let [err, fileStat] = await new Promise((resolve) => {
         fs.stat(fileRelPath, (err, stat) => { resolve([err, stat]); });
     });
+    if (err == null) {
+        err = await new Promise((resolve) => fs.access(fileRelPath, fs.R_OK, (err) => { resolve(err); }));
+    }
     if (err) {
-        rsp.writeHead(404, { 'Content-Type': "text/plain; charset=UTF-8" });
-        rsp.end(err.message);
+        return [null, null, null, {
+            code: 404,
+            header: { 'Content-Type': "text/plain; charset=UTF-8" },
+            message: err.message,
+        }];
+    }
+    return [fileStat, fileRelPath, decodedUrlPath, null];
+}
+
+const httpServer = http.createServer();
+
+httpServer.on('request', async function (req, rsp) {
+    const [fileStat, fileRelPath, decodedUrlPath, errRsp] = await parseUriToFileStat(req.url);
+    if (errRsp) {
+        rsp.writeHead(errRsp.code, errRsp.header);
+        rsp.end(errRsp.message);
         return;
     }
 
@@ -205,4 +226,82 @@ http.createServer(async function (req, rsp) {
         rsp.end();
         return;
     }
-}).listen(BIND_PORT, BIND_IP);
+});
+
+httpServer.on('upgrade', async function (req, socket) {
+    if (req.headers['upgrade'] !== 'websocket') {
+        socket.end("HTTP/1.1 403 Forbidden\r\n\r\n");
+        return;
+    }
+    const [fileStat, fileRelPath, decodedUrlPath, errRsp] = await parseUriToFileStat(req.url);
+    if (errRsp || !fileStat.isFile()) {
+        socket.end(`HTTP/1.1 403 Forbidden\r\n\r\n`);
+        return;
+    }
+    // Sec-WebSocket-Accept is calculated and responsed for proving this is not a cache
+    const secWebsocketAccept = require('crypto')
+        .createHash('SHA1')
+        .update(req.headers['sec-websocket-key'] + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+        .digest('base64');
+    socket.write(
+        "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" +
+        "Upgrade: WebSocket\r\n" +
+        "Connection: Upgrade\r\n" +
+        "Sec-WebSocket-Accept: " + secWebsocketAccept + "\r\n" +
+        "\r\n");
+
+    // websocket is ready, now wrap file updating data into websocket frames to send to client.
+    const tail = require('child_process').spawn("tail", ["-f", fileRelPath]);
+    console.log(`launch tail -f ${fileRelPath} process ${tail.pid} for websocket connection`);
+    tail.on('error', () => {
+        console.log(`close websocket for tail -f ${fileRelPath} process ${tail.pid} failed to spawn`);
+        socket.end();
+    });
+    tail.on('exit', () => {
+        tail.exited = true;
+        if (!socket.destroyed) {
+            console.log(`close websocket for lost tail -f ${fileRelPath} process ${tail.pid}`);
+            socket.end();
+        }
+    });
+    socket.on('data', () => {
+        // have to register 'data' event for 'end' event to work.
+        // we will not parse any incoming frames from client.
+    });
+    socket.on('end', () => {
+        socket.destroy();
+        if (!tail.exited) {
+            console.log(`kill tail -f ${fileRelPath} process ${tail.pid} for closed websocket`);
+            tail.kill();
+        }
+    });
+    tail.stdout.on('data', (stdoutChunk) => {
+        // websocket frame: https://www.rfc-editor.org/rfc/rfc6455
+        let buffer = null;
+        let payloadOffset = 0;
+        if (stdoutChunk.length < 126) {
+            buffer = Buffer.alloc(2);
+            buffer.writeUInt8(stdoutChunk.length, 1);
+            payloadOffset = 2;
+        } else if (stdoutChunk.length <= 65536) {
+            buffer = Buffer.alloc(2 + 2);
+            buffer.writeUInt8(126, 1);
+            buffer.writeUInt16BE(stdoutChunk.length, 2);
+            payloadOffset = 4;
+        } else {
+            buffer = Buffer.alloc(2 + 8);
+            buffer.writeUInt8(127, 1);
+            buffer.writeBigUint64BE(stdoutChunk.length, 2);
+            payloadOffset = 10;
+        }
+        // Write out the first byte, using opcode `2` to indicate that the message 
+        // payload is binary(although most of the time it is pure text)
+        buffer.writeUInt8(0b10000010, 0);
+        buffer = Buffer.concat([buffer, stdoutChunk]);
+        // might return false indicating underlying buffer is full, but it's ok to lose some frames
+        socket.write(buffer);
+    });
+});
+
+console.log(`starting http server at ${BIND_IP}:${BIND_PORT}...`);
+httpServer.listen(BIND_PORT, BIND_IP);
